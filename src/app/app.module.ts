6ee61212782f48981a1ab7ccd5ec128187b26b49
Etienne Bellenger <etienne.bellenger@gmail.com>
2020-07-11 01:36:00 -0400

    todo ordenado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
-<<<<<<< HEAD
-import { ContactoComponent } from './contacto/contacto.component';
-import { Top10productosComponent } from './top10productos/top10productos.component';
-import { LibroComponent } from './libro/libro.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InicioComponent } from './inicio/inicio.component';
-=======
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -33,7 +28,6 @@ import { CarritoComponent } from './carrito/carrito.component';
 
 
 
->>>>>>> refs/remotes/origin/master
 
 
 @NgModule({
@@ -43,15 +37,9 @@ import { CarritoComponent } from './carrito/carrito.component';
     CategoriaComponent,
     ContactoComponent,
     Top10productosComponent,
-<<<<<<< HEAD
     CarritoComponent,
-=======
-<<<<<<< HEAD
     LibroComponent,
     InicioComponent
-=======
->>>>>>> refs/remotes/origin/master
->>>>>>> 26957b372c31c489aff689f4453520ee643aa195
   ],
   imports: [
     BrowserModule,
